dadfaf283a9bb4a3d3daf8c906a3e4e356f8dd21
MaxChuloshnikov <Maks447@ukr.net>
2022-12-28 14:45:14 +0200

    addet Global Styles

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+    background-color: #191919;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: 600;
+    max-width: 1512px;
+    max-height: 982px;
+    }
+`
+
+export default GlobalStyle;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import GlobalStyle from './GlobalStyle.jsx';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='/moviebes-website'>
+      <GlobalStyle/>
       <App />
     </BrowserRouter>
   </React.StrictMode>
