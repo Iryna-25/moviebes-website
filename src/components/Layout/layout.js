80c4f1fd48a4fa5f66c499ffae6a50e808705c88
Iryna Shandar <iryna.shandar@gmail.com>
2023-01-01 17:38:45 +0200

    Slider

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -4,7 +4,9 @@ import { Outlet } from "react-router"
 
 export const Layout = () => {
     return (
-        <Box display="grid" gridTemplateColumns="200px 1fr">
+        <Box  
+        // display="grid" gridTemplateColumns="200px 1fr"
+        >
             <AppBar />
             <Outlet />
         </Box>
--- src/components/SliderHome/SliderHome.jsx
+++ src/components/SliderHome/SliderHome.jsx
@@ -1,9 +1,62 @@
-const SliderHome = () => {
-  return(
-    <div>
-      Place for slider
+import { useEffect, useState } from "react";
+import Slider from "react-slick";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+
+import "../SliderHome/SliderHome.css";
+import { getTrandingMovies } from "services/api";
+
+
+function SliderHome() {
+
+  const [trandingImg, setTrandingImg] = useState([]);
+
+  useEffect(()=>{
+    getTrandingMovies().then(data => {
+      setTrandingImg(data.results);
+    });
+  },[]);
+
+  const NextArrow = ({ onClick }) => {
+    return (
+      <div className="arrow next" onClick={onClick}>
+        <FaArrowRight />
+      </div>
+    );
+  };
+
+  const PrevArrow = ({ onClick }) => {
+    return (
+      <div className="arrow prev" onClick={onClick}>
+        <FaArrowLeft />
+      </div>
+    );
+  };
+
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const settings = {
+    infinite: true,
+    lazyLoad: true,
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    centerPadding: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    beforeChange: (current, next) => setImageIndex(next),
+  };
+
+  return (
+    <div className="A">
+      <Slider {...settings}>
+        {trandingImg.map((img, idx) => (
+          <div key={img.id} className={idx === imageIndex ? "slide activeSlide" : "slide"}>
+            <img src={`https://image.tmdb.org/t/p/w500${img.backdrop_path}`} alt={img} />
+          </div>
+        ))}
+      </Slider>
     </div>
-  )
+  );
 }
 
-export default SliderHome
+export default SliderHome;
