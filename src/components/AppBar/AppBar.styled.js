34f7aa9e5ac503f984a53090b8687e48e79104bd
MaxChuloshnikov <Maks447@ukr.net>
2023-01-05 00:05:29 +0200

    SideBar Complited
    Не могу создать папку с иконками и залить их в проект из фигмы. Наведение и активность кнопок разработаны без bofore i after. Так меньше кода, если не подойдет такой вариант то переделаю.

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -1,7 +1,39 @@
 import styled from 'styled-components';
 import { NavLink } from "react-router-dom";
 
+
+export const NavHeader = styled.header`
+background-color: ${({theme}) => theme.colors.sideBarBackground};
+border-radius: 0px 45px 45px 0px;
+display: flex;
+justify-content: center;
+`;
+
+export const NavContainer = styled.nav`
+  margin-top: 170px;
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  justify-content: start;
+  text-align: center;
+  margin-bottom: 50px;
+`;
+
 export const NavItem = styled(NavLink)`
+  color: ${({theme}) => theme.colors.primaryTextColor};
+  font-weight: ${({theme}) => theme.fontWeights.medium};
   display: flex;
   align-items: center;
+  text-decoration: none;
+  text-align: center;
+  padding: 40px 0px 37px 25px;
+  &:hover {
+    background-color: ${({theme}) => theme.colors.accentColor};
+    color: black;
+    opacity: 0.4;
+  }
+  
+  &.active {
+    border-right: 3px solid #3DD2CC;
+  }
 `;
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -6,6 +6,7 @@ export const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap');
 
 body {
+    background-color: ${({theme}) => theme.colors.pageBackground};
     margin: 0;
     font-family: 'Poppins', sans-serif;
     -webkit-font-smoothing: antialiased;
