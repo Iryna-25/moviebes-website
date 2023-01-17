e18ec644e063262a2f0b16f2e5c27d63cf0eda5a
Eldiwr <Eldiwr97@gmail.com>
2023-01-17 19:52:55 +0200

    Added ScrollButton

--- src/components/HomePageContent/HomePageContent.js
+++ src/components/HomePageContent/HomePageContent.js
@@ -1,12 +1,14 @@
 import { UpcomingList } from "components/UpcomingList/UpcomingList";
 import SliderHome from "components/Sliders/SliderHome/SliderHome";
+import { ScrollUpButton } from "components/ScrollUpButton/ScrollUpButton";
 
 export const HomePageContent = ({ movies, loadMore }) => {
     
     return (
         <section>
             <SliderHome/>
-            <UpcomingList movies={movies} loadMore={loadMore}/>
+            <UpcomingList movies={movies} loadMore={loadMore} />
+            <ScrollUpButton/>
         </section>
     );
 };
