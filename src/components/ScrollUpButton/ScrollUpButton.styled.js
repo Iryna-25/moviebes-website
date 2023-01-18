9b4879ef8bb3410760775bdad0b55bf5a0bd43ad
MaxChuloshnikov <Maks447@ukr.net>
2023-01-18 14:16:49 +0200

    AppButtonComplite
    I had to replace the button tag with a div, since I needed a container for the svg

--- src/components/ScrollUpButton/ScrollUpButton.styled.js
+++ src/components/ScrollUpButton/ScrollUpButton.styled.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
-export const ScrollButton = styled.button`
+export const ScrollButton = styled.div`
     position: fixed;
     bottom: 50px;
     right: 50px;
-    width: 150px;
-    background-color: white;
+    cursor: pointer;
+
+    & > svg {
+        height: 50px;
+        width: 50px;
+        margin: 0;
+        padding: 0;
+        transition: all 0.3s ease 0s;
+        color: ${({ theme }) => theme.colors.accentColor};
+        &:hover {
+        color: ${({ theme }) => theme.colors.accentHoverColor}; 
+        }
+    }
 `
